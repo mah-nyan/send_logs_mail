f9261d926eaa0e690a170d3fadae89e52fc3adad
mah-nyan <nlab.mahnyan@gmail.com>
2019-04-10 16:59:40 +0900

    圧縮ファイルのパスワード設定と、csvファイルの生成機能の追加

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
 # send_logs_mail
 WOWHoneypotのlogをメール送信するpython3スクリプトです!
 前日のlogを引っ張ってくるので、00:05等にcronで登録しておくと便利です。
-本文にサマリが表示され、圧縮されたファイルの中に、デコードしたlogが添付されます。
+本文にサマリが表示され、圧縮されたファイルの中に、デコードしたlogとcsvが添付されます。
+実行するサーバに7zのinstallが必要です。
 
 # mail sample
 ■2019-04-01のアクセス数は58件でした。
 
 ■送信元IPアドレスの数は 51件です。
 
-■メソッドの種別は以下です。
+■メソッドの一覧と件数は以下です。
 
-GET・・54件
-PUT・・0件
-POST・・0件
-HEAD・・1件
-CONNECT・・3件
-PROFFIND・・0件
-その他・・0件
+method GET 60 POST 1 CONNECT 1 Name: path, dtype: int64
 
-■送信元ipアドレスの一覧と件数は以下となります。
+■アクセスパス一覧と件数は以下です。
 
-1.2.3.4・・・2件
+path / 39 /manager/html 4 /favicon.ico 3 /struts2-rest-showcase/orders.xhtml 1 /sitemap.xml 1 /robots.txt 1 /tmUnblock.cgi 1 /index.do 1 /index.action 1 /version 1 /cms1/wp-login.php 1 /cms1//wp-json/wp/v2/users/ 1 /cms1//?author=1 1 /cms/wp-login.php 1 /cms//wp-json/wp/v2/users/ 1 /cms//?author=1 1 //ldskflks 1 /.well-known/security.txt Name: method, dtype: int64
 
-11.22.33.44・・・16件
+■添付ファイルのpasswordは、以下となっています。
 
-10.20.30.40・・・1件
+honeypot
--- send_logs_mail.py
+++ send_logs_mail.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 #WOWHoneypotの前日のlogをメール送付するpython3スクリプトです。
-#本文にはサマリが記載され、加工したlogが圧縮されて届きます。
+#本文にはサマリが記載され、加工したlogが圧縮されて届きます。(passwordはhoneypot)
+#圧縮時にsubprocessで7zipを呼び出すため、事前にインストールが必要です。
+#sudo apt install p7zip-full
+#sudo pip3 install pandas
 #2019-04-04 mah-nyan
 import smtplib
 from email.mime.text import MIMEText
@@ -10,10 +13,12 @@ from email.mime.multipart import MIMEMultipart
 import datetime
 import re
 import os
+import csv
 from os.path import basename
 import base64
-import zipfile
+import subprocess
 from collections import Counter
+import pandas as pd
 
 LOGIN_ADDRESS = 'your.smtp.server@login.address'
 ENVELOPE_ADDRESS = 'wowhoneypot@your.domain'
@@ -22,7 +27,7 @@ TO_ADDRESS = 'your@gmail.com'
 SUBJECT = 'WOWHoneypot1号のDailylog'
 yestarday = datetime.date.today() - datetime.timedelta(1)
 
-def create_body():
+def create_file():
     try:
         
         accesslog = open('/opt/wow/WOWHoneypot/log/access_log')
@@ -30,42 +35,24 @@ def create_body():
         accesslog.close
 
         logcount = 0
-        getcount = 0
-        putcount = 0
-        postcount = 0
-        headcount = 0
-        connectcount = 0
-        propfindcount = 0
         rawdata = "■アクセスlogのrawdataは以下となってます。\n\n"
         for line in lines:
             ###################################################################↓除外したいIPアドレス。例えば確認時にアクセスする自分のGIP等
             if line.find(yestarday.strftime("%Y-%m-%d")) >= 0 and line.find("1.2.3.4") < 0:
                 logcount += 1
-                if line.find("\"GET") >= 0:
-                    getcount += 1
-                if line.find("\"POST") >= 0:
-                    postcount += 1
-                if line.find("\"HEAD") >= 0:
-                    headcount += 1
-                if line.find("\"CONNECT") >= 0:
-                    connectcount += 1
-                if line.find("\"PUT") >= 0:
-                    putcount += 1
-                if line.find("\"PROPFIND") >= 0:
-                    propfindcount += 1    
                 rawdata += "-=-=" + str(logcount) + "件目のlog=-=-\n\n"
                 rawdata += line[:-1]
                 bunkatsu = line.rsplit(" ", 1)
                 decdata = (base64.decodestring(bunkatsu[-1].encode("ascii")).decode("utf8"))
                 rawdata += ("\n\n" + str(decdata) + "\n\n")
 
-        ac_csv = str()
+        ac_log = str()
         for line in lines:
             ###################################################################↓除外したいIPアドレス。例えば確認時にアクセスする自分のGIP等
             if line.find(yestarday.strftime("%Y-%m-%d")) >= 0 and line.find("1.2.3.4") < 0:
                 bunkatsu2 = line.rsplit(" ", 1)
                 decdata2 = (base64.decodestring(bunkatsu2[-1].encode("ascii")).decode("utf8"))
-                ac_csv += line.rsplit(" ", 1)[0] + decdata2  
+                ac_log += line.rsplit(" ", 1)[0] + decdata2  
 
         all_iplist =[]
         for line in lines:
@@ -74,12 +61,6 @@ def create_body():
                 all_ips = re.search("(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\s)", line)
                 all_iplist.append(all_ips.group())
                 unique_iplist = list(dict.fromkeys(all_iplist))
-
-        all_ipcount = Counter(all_iplist)
-        ip_data = "■送信元ipアドレスの一覧と件数は以下となります。\n\n"
-        for ip_key in all_ipcount.keys():
-            ip_val = all_ipcount[ip_key]
-            ip_data += str(ip_key) + "・・・" + str(ip_val) + "件\n\n"
 
         if os.path.exists("/opt/wow/WOWHoneypot/log/hunting.log"): 
             with open('/opt/wow/WOWHoneypot/log/hunting.log', "r") as huntinglog:
@@ -96,31 +77,103 @@ def create_body():
             huntingdata = ("■hunting.logは生成されていません。\n\n")
 
         body = "■" + yestarday.strftime("%Y-%m-%d") + "のアクセス数は" + str(logcount) + "件でした。\n\n"+ "■送信元IPアドレスの数は\n"\
-        + str(len(unique_iplist)) + "件です。\n\n" + "■メソッドの種別は以下です。\n\n" + " GET・・" + str(getcount) + "件\n" \
-        + " PUT・・" + str(putcount) + "件\n" + " POST・・" + str(postcount) + "件\n" + " HEAD・・" + str(headcount) + "件\n" \
-        + " CONNECT・・" + str(connectcount) + "件\n" +  " PROFFIND・・" + str(propfindcount) + "件\n" + " その他・・" \
-        + str(logcount - (getcount + putcount + postcount + headcount + connectcount + propfindcount)) + "件\n\n" + ip_data
+        + str(len(unique_iplist)) + "件です。\n\n"
 
         if not os.path.exists("./log/"):
             os.mkdir("./log/")
         
         csv = open("./log/access_" + yestarday.strftime("%Y-%m-%d") + ".log", "w")
-        csv.write(ac_csv)
+        csv.write(ac_log)
         csv.close
 
         f = open("./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".log", "w")
         f.write(huntingdata + rawdata)
         f.close
-
-        compfile = zipfile.ZipFile("./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".zip", "w", zipfile.ZIP_LZMA)
-        compfile.write("./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".log")
-        compfile.write("./log/access_" + yestarday.strftime("%Y-%m-%d") + ".log")
-        compfile.close()
-
+        
         return body
+
     except Exception as e:
         body = str(e)
         print = str(e)
+        return body
+
+
+def create_csv(body): 
+    try:
+        arg1 = "log/access_" + yestarday.strftime("%Y-%m-%d") + ".log"
+        log_file = open("./" + arg1)
+        lines = log_file.readlines()
+        log_file.close
+        csvdata = [["date", "time", "srcip", "method", "path"]]
+        for line in lines:
+            date = re.search ("\d{4}-\d{2}-\d{2}", line)
+            time = re.search ("\d{2}:\d{2}:\d{2}", line)
+            srcip = re.search ("]\s\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", line)
+            method = re.search ("(?<=\s\")\w+", line)
+            uri = re.search ("\s/\s|\s(/[\w/:%#\$&\?\(\)~\.=\+\-]+)(?=\sHTTP/\d\.\d\")", line)
+            uri2 = re.search ("\s(\w+[\w/:%#\$&\?\(\)~\.=\+\-]+)(?=\sHTTP/\d\.\d\")", line)
+            if uri is not None:
+                rawuri = str(uri.group()).lstrip( )
+                rawsrcip = str(srcip.group())
+                csvdata += [[date.group(), time.group(), rawsrcip.replace("] ", ""), method.group(), rawuri]]
+            if uri2 is not None:
+                rawuri2 = str(uri2.group()).lstrip( )
+                rawsrcip = str(srcip.group())
+                csvdata += [[date.group(), time.group(), rawsrcip.replace("] ", ""), method.group(), rawuri2]]
+        
+        if not os.path.exists("./log/"):
+            os.mkdir("./log/")            
+        
+        with open('./' + arg1 + ".csv", "w") as file:
+            writer = csv.writer(file, lineterminator='\n')
+            writer.writerows(csvdata)
+
+        return body
+
+    except Exception as e:
+        csvdata += [["error", "error", "error", "error", str(e)]]
+        print (str(e))
+        body += "※csv作成時にエラーが発生しています。\n\n"
+        return body
+
+
+def log_analyze(body):
+    try:
+        log_analyzed = str()
+        arg1 = "log/access_" + yestarday.strftime("%Y-%m-%d") + ".log"
+        df = pd.read_csv('./' + arg1 + ".csv",sep=",")
+        df.columns = ["date", "time", "srcip", "method", "path"]
+               
+        df_by_method = df.groupby("method")["path"].count().sort_values(ascending=False)
+        log_analyzed += "■メソッドの一覧と件数は以下です。\n\n"
+        log_analyzed += str(df_by_method) + "\n\n"
+        
+        df_by_uri = df.groupby("path")["method"].count().sort_values(ascending=False)
+        log_analyzed += "■アクセスパス一覧と件数は以下です。\n\n"
+        log_analyzed += str(df_by_uri) + "\n\n"
+        
+        body += log_analyzed
+
+        return body
+
+    except Exception as e:
+        body += "※log分析時にエラーが発生しています。\n\n"
+        return body
+
+
+def compress_files(body):
+    try:
+        cmd = "7z a -phoneypot " + "./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".7z " + "./log/access_" \
+            + yestarday.strftime("%Y-%m-%d") + ".log " + "./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".log " \
+                + "./log/access_" + yestarday.strftime("%Y-%m-%d") + ".log.csv"
+
+        subprocess.run(cmd.split())
+
+        body += "■添付ファイルのpasswordは、以下となっています。\n\nhoneypot\n\n"
+        return body
+
+    except Exception as e:
+        body += "※ファイル圧縮時にエラーが発生しています。\n\n"
         return body
 
 
@@ -134,7 +187,7 @@ def create_message(from_addr, to_addr, subject, body, mine):
     body = MIMEText(body)
     msg.attach(body)
 
-    path = "./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".zip"
+    path = "./log/honeypot_" + yestarday.strftime("%Y-%m-%d") + ".7z"
     with open(path, "rb") as f:
         part = MIMEApplication(
             f.read(),
@@ -160,7 +213,7 @@ def send(from_addr, to_addrs, msg):
 if __name__ == '__main__':
     to_addr = TO_ADDRESS
     subject = SUBJECT
-    body = create_body()
+    body = compress_files(log_analyze(create_csv(create_file())))
     mine={'type':'text','subtype':'comma-separated-values'}
     msg = create_message(ENVELOPE_ADDRESS, to_addr, subject, body, mine)
     send(ENVELOPE_ADDRESS, to_addr, msg)
